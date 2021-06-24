3208b3b1bf03a7b3123b21c323132a7184e9450b
susanee <comtrial97@gmail.com>
2021-06-24 11:03:39 +0900

    third

---
+++ lib/controller/home_controller.dart
@@ -0,0 +1,12 @@
+// on home init data
+
+import 'package:get/get_state_manager/get_state_manager.dart';
+
+class HomeController extends GetxController {
+  static HomeController get to => Get.find();
+
+  @overrider
+  void onInit(){
+    super.onInit()
+  }
+}
